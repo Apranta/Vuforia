2b1365504a386f4bbed96806ce5a3323c0711ff6
Ari Susanto <risusanto@students.ilkom.unsri.ac.id>
2017-11-30 22:01:44 +0700

    desain menu utama

---
+++ percobaan_31102017/Assets/Scripts/buttonHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class buttonHandler : MonoBehaviour {
+
+    public GameObject contactObj;
+    public GameObject beritaObj;
+    bool showContact;
+    bool showBerita;
+	// Use this for initialization
+	void Start () {
+        showContact = false;
+        showBerita = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        contactObj.SetActive(showContact);
+        beritaObj.SetActive(showBerita);
+	}
+
+    public void wisata()
+    {
+        SceneManager.LoadScene("percobaan_1");
+    }
+
+    public void kontak()
+    {
+        if (showBerita)
+            showBerita = false;
+        showContact = !showContact;
+    }
+
+    public void berita()
+    {
+        if (showContact)
+            showContact = false;
+        showBerita = !showBerita;
+    }
+}
