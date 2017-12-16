e144e53dcd46b1893184023009f768cac3b8b558
Ari Susanto <risusanto@students.ilkom.unsri.ac.id>
2017-12-16 23:27:27 +0700

    objek wisata, menu objek wisata

--- percobaan_31102017/Assets/Scripts/buttonHandler.cs
+++ percobaan_31102017/Assets/Scripts/buttonHandler.cs
@@ -8,18 +8,22 @@ public class buttonHandler : MonoBehaviour {
 
     public GameObject contactObj;
     public GameObject beritaObj;
+    public GameObject wisataObj;
     bool showContact;
     bool showBerita;
+    bool showWisata;
 	// Use this for initialization
 	void Start () {
         showContact = false;
         showBerita = false;
+        showWisata = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         contactObj.SetActive(showContact);
         beritaObj.SetActive(showBerita);
+        wisataObj.SetActive(showWisata);
 	}
 
     public void wisata()
@@ -34,11 +38,31 @@ public class buttonHandler : MonoBehaviour {
         showContact = !showContact;
     }
 
+    public void wisata_btn()
+    {
+        showWisata = !showWisata;
+    }
+
     public void berita()
     {
         beritaHandler.instance.createBerita();
         if (showContact)
             showContact = false;
         showBerita = !showBerita;
+    }
+
+    public void wisataAlam()
+    {
+        SceneManager.LoadScene("alam");
+    }
+
+    public void wisataBuatan()
+    {
+        SceneManager.LoadScene("buatan");
+    }
+
+    public void wisataSejarah()
+    {
+        SceneManager.LoadScene("sejarah-budaya");
     }
 }
---
+++ percobaan_31102017/Assets/Scripts/wisataHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wisataHandler : MonoBehaviour {
+
+    public GameObject[] objekWisata;
+    public GameObject position;
+    public static wisataHandler instance;
+
+    private GameObject wisataActive;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    public void create(int i)
+    {
+        wisataActive = objekWisata[i];
+        Instantiate(wisataActive,position.transform);
+    }
+
+    public void tutup()
+    {
+        Destroy(wisataActive);
+        try
+        {
+            Destroy(wisataActive);
+        }
+        catch
+        {
+
+        }
+    }
+}
