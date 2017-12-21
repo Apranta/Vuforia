334545dc5b82016d334f5ca1bc59744dd4d36a28
Ari Susanto <risusanto@students.ilkom.unsri.ac.id>
2017-12-21 15:33:16 +0700

    selesai

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -7,20 +7,6 @@ class Api extends MY_Controller {
     {
         parent::__construct();
         $this->load->model('Berita_m');
-
-        if (isset($_SERVER['HTTP_ORIGIN']))
-        {
-            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-            header('Access-Control-Allow-Credentials: true');
-            header('Access-Control-Max-Age: 86400'); // cache for 1 day
-        }
-        // Access-Control headers are received during OPTIONS requests
-        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS')
-        {
-            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-            exit(0);
-        }
     }
 	public function index()
 	{
@@ -34,7 +20,8 @@ class Api extends MY_Controller {
     }
 
     public function testimoni(){
-        /*if($this->POST('testimoni')){
+        $this->load->model('Testimoni_m');
+        if($this->POST('testimoni')){
             $data = [
                 'nama_guest' => $this->POST('nama'),
                 'email_guest' => $this->POST('email'),
@@ -51,18 +38,6 @@ class Api extends MY_Controller {
         $response = [
             'status' => 'gagal'
         ];
-        echo json_encode($response);*/
-        $data = [
-            'nama_guest' => $this->POST('nama'),
-            'email_guest' => $this->POST('email'),
-            'isi' => $this->POST('testimoni')
-        ];
-        $this->Testimoni_m->insert($data);
-        $response = [
-            'status' => 'ok',
-            'pesan' => 'Testimoni berhasil disubmit'
-        ];
         echo json_encode($response);
-        exit;
     }
 }
